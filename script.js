779c3624c31d4646a70183adffaadb04f4db6397
Grinmak <grinmak@gmail.com>
2022-06-08 18:43:47 +0300

    Replace pseudocode with few comments

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ const userInput = () => prompt('Let\'s play Rock, Paper, Scissors! Type in your 
 let userAnswer = userInput();
 let userLowCaseAnswer = userAnswer.toLowerCase();
 
-/* let userInput = prompt('Let\'s play Rock, Paper, Scissors! Type in your choice:'); */ //function more useful
-// it can be only rock or paper or scissors
-// chech if input is not an integer or empty string
-// alert if input integer or empty
+// check if user type in right words
 function checkUserInput ()  {
     if (userLowCaseAnswer === 'rock' || userLowCaseAnswer === 'paper' || userLowCaseAnswer === 'scissors') {
         return userLowCaseAnswer;
@@ -16,20 +13,13 @@ function checkUserInput ()  {
         alert ('You can use only rock, paper or scissors! Try again!');
         userInput();
 }}
-
 let playerSelection = checkUserInput();
 
 // create function computerPlay that will randomly shoose one of the values like rock, paper or scissors
 const computerPlay = () => gameItems[Math.floor(Math.random() * gameItems.length)];
 let computerSelection  = computerPlay ();
 
-// compare user input and computerPlay function
-// make few conditions that return true if user beats computer and false overwise. 
-// conditions should be: rock>scissors; paper>rock; scissors>paper
-// if user win, alert: "You win! 'here should be user input' beat 'here computer choice'"
-// if user lose, alert: "You Lose! here computer choice' beat 'here should be user input'"
-// alert 'even score' if user input and computerPlay has equal values
-// use capital first letter in result alert
+//compare user and computer and show who is the winner
 function playRound (userSel, compSel){
     return userSel === 'rock' && compSel === 'scissors' ?  `You win! Rock beats Scissors!`
     :  userSel === 'scissors' && compSel === 'paper' ? `You win! Scissors beats Paper!`
@@ -38,3 +28,5 @@ function playRound (userSel, compSel){
     : `You lose! ${compSel} beats ${userSel}!`
    }
 alert (playRound(playerSelection,computerSelection ))
+
+//function to choose winner after five rounds
