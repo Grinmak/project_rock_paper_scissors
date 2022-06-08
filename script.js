ce9234ae94d2d6b913985c61fa1a8c20e79367aa
Grinmak <grinmak@gmail.com>
2022-06-08 14:24:33 +0300

    Make pseudocode with comments

---
+++ script.js
@@ -0,0 +1,12 @@
+// user input
+// it can be only rock or paper or scissors
+// chech if input is not an integer or empty string
+// alert if input integer or empty
+// create function computerPlay that will randomly shoose on of the values like rock, paper or scissors
+// compare user input and computerPlay function
+// make few conditions that return true if user beats computer and false overwise. 
+// conditions should be: rock>scissors; paper>rock; scissors>paper
+// if user win, alert: "You win! 'here should be user input' beat 'here computer choice'"
+// if user lose, alert: "You Lose! here computer choice' beat 'here should be user input'"
+// alert 'even score' if user input and computerPlay has equal values
+// use capital first letter in result alert
