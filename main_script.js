6aa8ec1194a5151ec51c0c67ff846c36912203b6
Grinmak <grinmak@gmail.com>
2022-06-10 16:41:51 +0300

    Remove empty lines and ussles parts of code inside comment

--- main_script.js
+++ main_script.js
@@ -8,17 +8,14 @@ const game = () => {
     const gameCount = 4;
  
 //offer user to play game
-function oneRound (){
-    function userInputFn (){
-    userInput = prompt ('Let\'s play Rock, Paper, Scissors! What you\'ll chose?').toLowerCase();
+    function oneRound (){
+        function userInputFn (){
+        userInput = prompt ('Let\'s play Rock, Paper, Scissors! What you\'ll chose?').toLowerCase();
     }
 //call function to check if user's answer is correct
     function userAnswer () {      
         if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
             userInput = userInput[0].toUpperCase() + userInput.substring(1) ;
-/*         }else if (userInput === '' || userInput !== NaN){
-            alert ('See you!'); 
-            clear(); */
         }else { 
             alert('Wrong word! Try again!');
             userAnswer(userInputFn());
@@ -31,26 +28,24 @@ function oneRound (){
     }
     computerPlay ();
 //compare answers
-function gameBattle (userFirst, pcSecond){
-    if ((userFirst === 'rock' && pcSecond === 'scissors') 
+    function gameBattle (userFirst, pcSecond){
+        if ((userFirst === 'rock' && pcSecond === 'scissors') 
         || (userFirst === 'scissors' && pcSecond === 'paper')
         || (userFirst === 'paper' && pcSecond === 'rock')) {
             alert (`You win! ${userFirst} beats ${pcSecond}!`);
             ++playerScore;
-    }else if (userFirst === pcSecond) {
-        alert (`Hmm... you both choose ${pcSecond}.`);
-    }else alert (`You lose! ${pcSecond} beats ${userFirst}!`);
-        computerScore++;
-}
-gameBattle (userInput, computerInput);
-}
+        }else if (userFirst === pcSecond) {
+            alert (`Hmm... you both choose ${pcSecond}.`);
+        }else alert (`You lose! ${pcSecond} beats ${userFirst}!`);
+            computerScore++;
+    }
+    gameBattle (userInput, computerInput);
+    }
 
-
-
-for (let i=0; i < gameCount; i++){
+    for (let i=0; i < gameCount; i++){
         //console.log(i);
         oneRound();
-} 
+    } 
 //winner function
 function winnerFn() {
         if (playerScore > computerScore) {
@@ -60,6 +55,5 @@ function winnerFn() {
 
 winnerFn();
 }
-
 
 game ();
