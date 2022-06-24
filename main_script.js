95e73ac041817c3a140a2af07252a2453ff93f0a
Grinmak <grinmak@gmail.com>
2022-06-24 19:02:09 +0300

    fix scores and add final result when 5 points achieved

--- main_script.js
+++ main_script.js
@@ -5,6 +5,7 @@ const game = () => {
     let computerScore; */
     let computerInput;
     let userInput;
+    
 
 //button nodes
 const body = document.querySelector('body');
@@ -12,38 +13,50 @@ const divButton = document.createElement('div');
 const btnRock = document.createElement('button');
 const btnPaper = document.createElement('button');
 const btnScissors = document.createElement('button');
-const divResult = document.createElement('div');
 divButton.append(btnRock, btnPaper, btnScissors);
 body.appendChild(divButton);
 btnRock.textContent = 'Rock';
 btnPaper.textContent = 'Paper';
 btnScissors.textContent ='Scissors';
 divButton.setAttribute('style', 'display:flex;  justify-content: center; padding: 50px 0');
-divResult.setAttribute('style', 'background-color: #d3d3d3;  height: 20rem' );
+
+//result and score sector
+const divResult = document.createElement('div');
+divResult.setAttribute('style', 'background-color: #d3d3d3;  min-height: 350px; align-items: center; display: flex; flex-flow:column; border-radius:15px' );
 body.appendChild(divResult);
 const divScore = document.createElement('div');
 let playerScore = document.createElement('p');
 let computerScore = document.createElement('p');
-playerScore.textContent = 0;
-computerScore.textContent = 0;
 divScore.append(playerScore, computerScore);
 body.appendChild(divScore);
-divScore.setAttribute('style', 'display:flex; justify-content: space-evenly')
+divScore.setAttribute('style', 'display:flex; justify-content: space-evenly');
+playerScore.textContent = 0;
+computerScore.textContent = 0;
+
+
 //events
  const allButton = Array.from(document.querySelectorAll('button'));
  allButton.forEach(btn => btn.addEventListener('click', btnSelector));
 
  function btnSelector (event){
     userInput = event.srcElement.innerText;
-    oneRound(userInput); 
+    if(Number(playerScore.textContent) < 5 && Number(computerScore.textContent < 5)){
+    oneRound(userInput)
+    } else return ;
     //console.log(userInput);
  }
 
 
-function oneRound (user){
-    const matchResult = document.createElement('h3');
+function oneRound (user){   
+    const matchResult = document.createElement('h4');
     divResult.appendChild(matchResult); 
+    const finalScore = document.createElement('div');
+    body.appendChild(finalScore);
+   // finalScore.textContent = 'lllllll';
+    finalScore.setAttribute('style', 'text-align: center; font-size: 1.5em; font-weight: bold')
     computerInput = gameWords[Math.floor(Math.random() * gameWords.length)];
+    
+
     if ((user == 'Rock' && computerInput == 'Scissors') 
     || (user == 'Scissors' && computerInput == 'Paper')
     || (user == 'Paper' && computerInput == 'Rock')) {
@@ -52,21 +65,37 @@ function oneRound (user){
         }else if (user === computerInput) {
             matchResult.textContent = `Hmm... you both choose ${computerInput}.`;
             
-        }else matchResult.textContent =`You lose! ${computerInput} beats ${user}!`;
+        }else {
+            matchResult.textContent =`You lose! ${computerInput} beats ${user}!`;
             ++computerScore.textContent; 
-    }                   
+        };
+        winnerFn();   
+        function winnerFn(/* a,b */) {
+            /*         let user = Number(a);
+                    let pc = Number(b); */
+                    if (Number(playerScore.textContent) > Number(computerScore.textContent) &&  Number(playerScore.textContent) == 5) {
+                        finalScore.textContent = `You win ${playerScore.textContent} to ${computerScore.textContent}!`;
+                        return;
+                    }else if(Number(playerScore.textContent) < Number(computerScore.textContent) && Number(computerScore.textContent) == 5){
+                         finalScore.textContent = `You lose!`;
+                         return;
+                    }     
+                }
+
+
+          
+    }   
+        
+    
+    //winner function
+
 }
 
 
 
-//winner function
-/* function winnerFn() {
-        if (playerScore > computerScore) {
-            alert (`You win ${playerScore} to ${computerScore}!`)
-        }else alert(`You lose!`);
-    }       
+  
 
-winnerFn(); */
+
 
 
 game ();
